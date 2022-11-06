e0883680e9793de1591846a2987e92454ec1f83c
geo <tp53.geo@gmail.com>
2022-11-06 06:36:02 +0100

    add check for smp configuration before launch

--- main/settings.py
+++ main/settings.py
@@ -155,11 +155,20 @@ AUTH_USER_MODEL = "user.CustomUser"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "XXXX"
-EMAIL_HOST_PASSWORD = "XXXX"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+if not EMAIL_HOST_PASSWORD or not EMAIL_HOST_USER:
+    print(
+        "SMTP NOT CONFIGURED : Please fill EMAIL_HOST_PASSWORD & EMAIL_HOST_USER located in main/settings.py"
+    )
+
+    if not DEBUG:
+        exit()
+
 
 EMAIL_VALIDATION_URL = "http://localhost:3000/email-confirmation/"
 PASSWORD_RESET_URL = "http://localhost:3000/password-reset/"
